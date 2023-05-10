4d08b5614ade4dafe3c0d4e4bc7e377763600b3e
Steven Zarichney <szarichney@hard-line.com>
2023-05-10 06:40:54 -0400

    auto unscubscribe on destroy event

--- client/components/app/app.component.ts
+++ client/components/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WebsocketService } from '../../services/websocket.service';
 import { v4 as uuidv4 } from 'uuid';
+import { AutoUnsubscribe } from '../../decorators/unsubscribe.decorator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+@AutoUnsubscribe('messageSubscription')
 export class AppComponent implements OnInit, OnDestroy {
   private messageSubscription!: Subscription;
   readonly id: string = uuidv4();
@@ -25,7 +27,8 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     // this.websocketService.disconnect();
-    this.messageSubscription.unsubscribe();
+    // No need to manually unsubscribe here, the decorator will handle it
+    // this.messageSubscription.unsubscribe();
   }
 
   public sendMessage(message: string): void {
